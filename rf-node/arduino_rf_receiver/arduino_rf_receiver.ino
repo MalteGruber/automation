e2fea6eb539dceb64fe561e1e665f2eb25ac4729
Malte Gruber <malte_gruber@hotmail.com>
2018-07-11 19:36:39 +0200

    Add files via upload

---
+++ rf-node/arduino_rf_receiver/arduino_rf_receiver.ino
@@ -0,0 +1,32 @@
+/*
+  Simple example for receiving
+  
+  https://github.com/sui77/rc-switch/
+*/
+
+#include "RCSwitch.h"
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+  Serial.begin(115200);
+
+  int pullupPin=6;
+  pinMode(pullupPin,OUTPUT);  
+  digitalWrite(pullupPin,HIGH);
+  pinMode(3,OUTPUT);
+  mySwitch.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2
+}
+
+void loop() {
+  if (mySwitch.available()) {
+    
+    Serial.print("{\"val\":");
+    Serial.print( mySwitch.getReceivedValue() );
+    Serial.print(",\"proto\":");
+    Serial.print( mySwitch.getReceivedProtocol() );
+    Serial.print("}\r\n");
+    digitalWrite(3,1);delay(20);digitalWrite(3,0);
+    mySwitch.resetAvailable();
+  }
+}
